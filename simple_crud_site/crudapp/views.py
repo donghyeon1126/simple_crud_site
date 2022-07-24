c20666d4c0eb69998c62b8cac90a4a15c84d4349
donghyeon1126 <handonghyeon1126@gmail.com>
2022-07-24 15:34:23 +0900

    add read feature

--- simple_crud_site/crudapp/views.py
+++ simple_crud_site/crudapp/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.utils import timezone
 from crudapp.models import Posts
 
 def index(request):
     if request.method == "GET":
+        datas = Posts.objects.order_by("-date")
+        if len(datas) != 0:
+            return render(request, "index.html", context={"posts":datas})
+        else:
             return render(request, "index.html")
 
 def create(request):
@@ -16,4 +20,12 @@ def create(request):
         content = request.POST.get("content")
         p = Posts(title=title, author=author, content=content, date=timezone.now())
         p.save()
-        return HttpResponse("<h1>success</h1>")
+        return redirect('/')
+
+def read(request):
+    try:
+        id = int(request.GET.get('id'))
+        article = Posts.objects.filter(id=id)[0]
+    except:
+        return redirect('/')
+    return render(request, "read.html", context={"article":article})
