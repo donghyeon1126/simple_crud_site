e6c872f36c7e624345af9d32bbb9d525c99c0290
donghyeon1126 <handonghyeon1126@gmail.com>
2022-07-25 16:37:47 +0900

    merge main branch

--- simple_crud_site/crudapp/models.py
+++ simple_crud_site/crudapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 class Posts(models.Model):
     title = models.CharField(max_length=30)
-    author = models.CharField(max_length=10)
     content = models.TextField()
     date = models.DateTimeField()
+    author = models.CharField(max_length=10)
+    author_password = models.TextField()
--- simple_crud_site/crudapp/views.py
+++ simple_crud_site/crudapp/views.py
@@ -1,24 +1,26 @@
+import hashlib
 from django.shortcuts import render,redirect
-from django.http import HttpResponse
 from django.utils import timezone
 from crudapp.models import Posts
 
 def index(request):
-    if request.method == "GET":
-        datas = Posts.objects.order_by("-date")
+    if request.method == 'GET':
+        datas = Posts.objects.order_by('-date')
         if len(datas) != 0:
-            return render(request, "index.html", context={"posts":datas})
+            return render(request, 'index.html', context={'posts':datas})
         else:
-            return render(request, "index.html")
+            return render(request, 'index.html')
 
 def create(request):
-    if request.method == "GET":
-        return render(request, "create.html")
-    if request.method == "POST":
-        title = request.POST.get("title")
-        author = request.POST.get("author")
-        content = request.POST.get("content")
-        p = Posts(title=title, author=author, content=content, date=timezone.now())
+    if request.method == 'GET':
+        return render(request, 'create.html')
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author = request.POST.get('author')
+        author_password = request.POST.get('author_password')
+        content = request.POST.get('content')
+        author_password = hashlib.sha256(author_password.encode()).hexdigest()
+        p = Posts(title=title, content=content, date=timezone.now(), author=author, author_password=author_password)
         p.save()
         return redirect('/')
 
@@ -28,4 +30,39 @@ def read(request):
         article = Posts.objects.filter(id=id)[0]
     except:
         return redirect('/')
-    return render(request, "read.html", context={"article":article})
+    return render(request, 'read.html', context={'article':article, 'host':request.get_host()})
+
+def delete(request):
+    if request.method == "GET":
+        article = Posts.objects.get(id=id)
+        return render(request, "update.html", context={"article":article})
+    author = request.POST.get('author')
+    author_password = hashlib.sha256(request.POST.get('author_password').encode()).hexdigest()
+    id = int(request.POST.get('id'))
+    article = Posts.objects.filter(id=id)[0]
+    if author_password == article.author_password and author == article.author:
+        article.delete()
+        return redirect('/')
+    else:
+        context = {'article':article, 'errormsg':'The password or nickname does wrong'}
+        return render(request, 'read.html', context=context)
+
+def update(request,id):
+    if request.method == 'GET':
+        article = Posts.objects.get(id=id)
+        return render(request, "update.html", context={"article":article})
+
+    if request.method == "POST":
+        author = request.POST.get('author')
+        author_password = hashlib.sha256(request.POST.get('author_password').encode()).hexdigest()
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Posts.objects.get(id=id)
+        if author == article.author and author_password == article.author_password:
+            article.title = title
+            article.content = content
+            article.save()
+            return redirect('/read?id='+str(id))
+        else:
+            context = {'article':article, 'errormsg':'the password or nickname does wrong'}
+            return render(request, 'update.html', context=context)
