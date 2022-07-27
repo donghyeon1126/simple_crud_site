e5a15d799d8555382a5a1b5ad73de1eea6303522
donghyeon1126 <handonghyeon1126@gmail.com>
2022-07-27 17:05:32 +0900

    add comment table

--- simple_crud_site/crudapp/models.py
+++ simple_crud_site/crudapp/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+
 class Posts(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
     date = models.DateTimeField()
     author = models.CharField(max_length=10)
     author_password = models.TextField()
+
+class Comments(models.Model):
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    comment_content = models.TextField()
+    comment_date = models.DateTimeField()
